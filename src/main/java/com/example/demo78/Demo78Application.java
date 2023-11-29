8356a1be86981a396a4432dc2f5bd0496aa9a5be
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 16:31:24 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo78/Demo78Application.java
@@ -0,0 +1,13 @@
+package com.example.demo78;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo78Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo78Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo78/Demo78ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo78;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo78ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
